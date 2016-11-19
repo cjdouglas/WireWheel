b4419dd9064a12a6f0f0a2828c631c81b5cdf753
cjdouglas <chris.douglas.210@gmail.com>
2016-11-18 22:33:00 -0600

    Added documentation to listing class

--- app/src/main/java/com/wirewheel/listings/Listing.java
+++ app/src/main/java/com/wirewheel/listings/Listing.java
@@ -48,61 +48,99 @@ public class Listing {
         this.link = link;
     }
 
+    /**
+     * Returns the title of this listing
+     * @return The title of this listing
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of this listing
+     * @param title The title to name this listing
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns the mileage of this listing
+     * @return The mileage of this listing
+     */
     public String getMileage() {
         return mileage;
     }
 
+    /**
+     * Sets the mileage of this listing
+     * @param mileage The mileage to set
+     */
     public void setMileage(String mileage) {
         this.mileage = mileage;
     }
 
+    /**
+     * Returns the price of this listing
+     * @return The price of this listing
+     */
     public String getPrice() {
         return price;
     }
 
+    /**
+     * Sets the price of this listing
+     * @param price The price to set
+     */
     public void setPrice(String price) {
         this.price = price;
     }
 
+    /**
+     * Returns the text of the listing
+     * @return The text of this listing
+     */
     public ArrayList<String> getText() {
         return text;
     }
 
+    /**
+     * Sets the text of this listing
+     * @param text The text to set
+     */
     public void setText(ArrayList<String> text) {
         this.text = text;
     }
 
+    /**
+     * Returns the link to the key image of this listing
+     * @return The link to the key image of this listing
+     */
     public String getKeyImageLink() {
         return keyImageLink;
     }
 
+    /**
+     * Sets the key image link of this listing
+     * @param keyImageLink The link to the key image
+     */
     public void setKeyImageLink(String keyImageLink) {
         this.keyImageLink = keyImageLink;
     }
 
+    /**
+     * Returns the string representing all links to images separated by a "|"
+     * @return a string containing all links to images for that listing separated by "|"
+     */
     public String getImageLinks() {
         return imageLinks;
     }
 
+    /**
+     * Sets the string representing the image links for this listing
+     * @param imageLinks the string containing image links to set for this listing
+     */
     public void setImageString(String imageLinks) {
         this.imageLinks = imageLinks;
-    }
-
-    /*
-    public void addImage(String link) {
-        imageLinks.add(link);
-    }
-    */
-
-    public void addTextLine(String line) {
-        text.add(line);
     }
 }
